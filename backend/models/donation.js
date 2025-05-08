37b0c6761232446784c7464572d2e4928f0c6333
vivek <vivek132002@gmail.com>
2025-05-08 21:27:57 +0530

    added Donation model schema to backend for managing donation data

--- backend/models/donation.js
+++ backend/models/donation.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
 
+const DonationSchema = new mongoose.Schema({
+  donor_name: String,
+  pan: String,
+  aadhaar: String,
+  employer: String,
+  dob: String,
+  address: String,
+  city: String,
+  state: String,
+  pincode: String,
+  gender: String,
+  mobile: String,
+  email: String,
+  donation_amount: Number,
+  support_causes: String,
+  other_initiative: String,
+  feedback: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Donation', DonationSchema);
