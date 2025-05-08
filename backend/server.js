bbe9afac8f87c08fc06b81917e660e713a7a6458
vivek <vivek132002@gmail.com>
2025-05-08 21:34:08 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ require('dotenv').config();
 const Donation = require('./models/donation');
 
 const app = express();
-app.use(cors({
-  origin: 'https://ikshanfoundation.com' 
-}));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
